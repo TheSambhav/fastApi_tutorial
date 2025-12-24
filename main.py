17c7992149ce0b02a68777253fb9ec587a3d965c
TheSambhav <jainsambhav2021@gmail.com>
2025-12-24 05:43:29 +0530

    day03

--- main.py
+++ main.py
@@ -11,3 +11,8 @@ def hello():
 @app.get("/about")
 def about():
     return {'message': 'This is a FastAPI practise project.'}     
+
+
+@app.get("/contact")
+def contact():
+    return {'message': 'Contact us at contact@example.com'}
