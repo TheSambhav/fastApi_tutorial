66e798a1c85a8c676a75e18f8024672d40ca0295
TheSambhav <jainsambhav2021@gmail.com>
2025-12-28 19:40:11 +0530

    Day_04

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 
+import json
+
+
 app = FastAPI()
+
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+
+    return data 
+   
+
+
 
 @app.get("/")
 
@@ -25,3 +37,14 @@ def help():
 @app.get("/status" )
 def status():
     return {'message': 'Server is running successfully'}
+
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str):
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    return {'error': 'Patient not found'}
+
+
